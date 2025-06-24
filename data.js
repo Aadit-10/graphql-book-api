c919fb485131fe46b122a85d79b5e02765e001c7
Aadit <aaditk777@gmail.com>
2025-06-24 17:54:22 +0530

    First Commit

---
+++ data.js
@@ -0,0 +1,11 @@
+const books = [
+  { id: "1", title: "1984", authorId: "1" },
+  { id: "2", title: "Animal Farm", authorId: "1" },
+];
+
+const authors = [
+  { id: "1", name: "George Orwell" },
+  { id: "2", name: "J.K. Rowling" },
+];
+
+module.exports = { books, authors };
